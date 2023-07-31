51ef37e54bd197a919994c28dabffaad67095d96
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-07-31 11:22:01 +0530

    feat: setTimeout doesnot execute after time we are passing.It will execute after stack is empty

--- setTimeout.js
+++ setTimeout.js
@@ -1,1 +1,35 @@
+function colorLog(message, color) {
+  color = color || 'black'
+
+  switch (color) {
+    case 'success':
+      color = 'Green'
+      break
+    case 'info':
+      color = 'DodgerBlue'
+      break
+    case 'warning':
+      color = 'Orange'
+      break
+    default:
+      color = color
+  }
+
+  console.log('%c' + message, 'color:' + color)
+}
+
+function delayFor2Second() {
+  while (Date.now() < time + 2000) {}
+}
+
 const time = Date.now()
+
+colorLog('Log me first', 'success')
+
+setTimeout(() => {
+  colorLog('Log me third', 'warning')
+}, 1000)
+
+delayFor2Second()
+
+colorLog('Log me second', 'info')
