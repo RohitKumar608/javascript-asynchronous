09145dce2f1890eab1759dad2616b88db72c2bc1
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-08-06 13:31:03 +0530

    feat: when async createFlow first run and when the promise and await it's pause the execution context and run other global synchronous code once done then go in to microtask queue and check the callstack is empty or not if empty then execution start from the previous pause

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,23 +1,10 @@
-function createFlow() {
+async function createFlow() {
   console.log('createFlow first')
-  return fetch('https://jsonplaceholder.typicode.com/posts')
-  // console.log(futureData)
-  // return futureData
+  const futureData = await fetch('https://jsonplaceholder.typicode.com/posts')
+  console.log(futureData)
+  return futureData
 }
 
-function createFlow1() {
-  console.log('createFlow-1 first')
-  return fetch('https://jsonplaceholder.typicode.com/posts')
-  // console.log(futureData)
-  // return futureData
-}
-
-createFlow().then((res) => console.log(res, '1'))
-createFlow1().then((res) => console.log(res, '2'))
-
-// function delayForSecond() {
-//   const time = Date.now()
-//   while (Date.now() < time + 2000) {}
-// }
+createFlow()
 
 console.log('Me second')
